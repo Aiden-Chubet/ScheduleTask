a3bc889b74e58e703d7d96ea0c5f5411b1d5108d
ach-u-be14 <147650530+ach-u-be14@users.noreply.github.com>
2023-10-17 15:45:25 -0400

    Script

---
+++ SchedulingTask.ps1
@@ -0,0 +1,5 @@
+﻿$Trigger = New-ScheduledTaskTrigger -Daily -At 3am
+$Action = New-ScheduledTaskAction -Execute “C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe” -Argument '-NoProfile -ExecutionPolicy Bypass -File "C:\Script\RWB.ps1"'
+$User = "NT AUTHORITY\SYSTEM"
+$Description = "Powershell Script"
+Register-ScheduledTask -Action $Action -TaskName "PowerShell Script" -Trigger $Trigger -User $User -Description $Description -Force -RunLevel Highest
